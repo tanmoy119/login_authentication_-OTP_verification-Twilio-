ef6488cb8a558398951ed4b164665d6e312ff5f7
unknown <srroy119@gmail.com>
2022-02-27 21:17:02 +0530

    .env commit

--- src/app.js
+++ src/app.js
@@ -39,3 +39,33 @@ app.use(mainRouter);
 app.listen(port,()=>{
     console.log(`server listen at port ${port}`);
 })
+
+
+
+/* Post man
+
+signup..
+1.http://localhost:3000/register/sendotp?number=91YOUR MOBILE NUMBER&channel=sms
+body{
+    {
+	
+	"name":"YOUR NAME",
+	"userid":"YOUR USER ID",
+	"email":"YOUR EMAIL",
+	"number":"91YOUR MOBILE NUMBER",
+	"password":"Tanmoy1234"
+	
+}
+}
+2.http://localhost:3000/register/verifyotp?number=91YOUR MOBILE NUMBER&code=YOUR OTP
+
+login...
+1.http://localhost:3000/login
+
+forgot..
+1.http://localhost:3000/forget/sendotp?number=91YOUR MOBILE NUMBER&channel=sms
+2.http://localhost:3000/forget/verifyotp?number=91YOUR MOBILE NUMBER&code=YOUR OTP&newpassword=1234
+
+
+
+*/
